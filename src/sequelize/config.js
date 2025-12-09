570b0e7cf98dce0223772c9fc69921c02792ce85
Chux-Dennis <chuksabosi63@gmail.com>
2025-12-09 15:46:54 +0100

    Fixed SSL db connection setting rejectUnauthorized:false 2

--- src/sequelize/config.js
+++ src/sequelize/config.js
@@ -1,6 +1,52 @@
-const fs = require("fs");
-const dotenv = require("dotenv");
-dotenv.config();
+// const fs = require("fs");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT || 4000,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       ssl: {
+//         require:true,
+//         ca: fs.readFileSync(process.env.CA_PATH) ,
+//           rejectUnauthorized: false
+//       }
+//     }
+//   }
+// };
+
+
+
+
+const dotenv = require("dotenv")
+const detectEnvironment = process.env.NODE_ENV
+dotenv.config()
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        //Set the property to true on deployment and false if locally
+        // rejectUnauthorized:  (!detectEnvironment || detectEnvironment === "production") ? false : true,
+        rejectUnauthorized: true
+      },
+    },
+  },
+};
+
+
+require('dotenv').config();
 
 module.exports = {
   development: {
@@ -8,14 +54,37 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 4000,
-    dialect: "mysql",
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
     dialectOptions: {
       ssl: {
-        require:true,
-        ca: fs.readFileSync(process.env.CA_PATH) ,
-          rejectUnauthorized: false
-      }
-    }
-  }
+        require: true,
+        rejectUnauthorized: false, // use false for local migrations
+      },
+    },
+  },
+  // test: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASS,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   dialect: 'mysql',
+  //   port: process.env.DB_PORT || 3306,
+  // },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // use false for local migrations
+      },
+    },
+  },
 };
